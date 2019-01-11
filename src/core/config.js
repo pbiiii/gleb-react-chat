483cea84215937be008d58f5fa2aabad2767fa03
Gleb <kosgleb@gmail.com>
2019-01-11 11:19:53 +0700

    changed production api url

--- src/core/config.js
+++ src/core/config.js
@@ -3,6 +3,6 @@ export default {
         ? 'https://dogecodes-chat-api.herokuapp.com/v1'
         : 'http://localhost:9000/v1',
     SOCKETS_URL: process.env.NODE_ENV === 'production'
-        ? 'ws://dogecodes-chat-api.herokuapp.com/'
+        ? 'wss://dogecodes-chat-api.herokuapp.com/'
         : 'ws://localhost:9000',
 };
